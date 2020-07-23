291020c2bac95efd46a840ab4abc64bf1d82c4e2
Rafael Flores <fita500@gmail.com>
2020-07-22 21:32:44 -0600

    Add AutoPlay plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,9 +1,22 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+    
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    })
 }
 
 MediaPlayer.prototype.play = function() {
     this.media.play();
+}
+
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
 }
 
 MediaPlayer.prototype.pause = function() {
