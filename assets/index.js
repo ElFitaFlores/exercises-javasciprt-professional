4b1a3c1edc558624a583388abe58a858445b00c4
Rafael Flores <fita500@gmail.com>
2020-09-08 19:51:53 -0600

    Add handle intersection exercises

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,12 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
-const video = document.querySelector('video');
-const button = document.querySelector('button');
+const video = document.querySelector("video");
+const button = document.querySelector("button");
 
-const player = new MediaPlayer({el: video, plugins: [new AutoPlay()]});
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
 button.onclick = () => player.togglePlay();
