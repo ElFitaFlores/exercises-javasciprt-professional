5a5bce0c2adf15ca6262d6c72de850943338c813
Rafael Flores <fita500@gmail.com>
2020-09-15 20:23:06 -0600

    Add data types

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -6,3 +6,34 @@ function add(a: number, b: number) {
 
 const sum = add(2, 4);
 console.log(sum);
+
+// boolean
+let muted: boolean = true;
+
+// numeros
+let numerador: number = 42;
+let denominador: number = 6;
+let resultado = numerador / denominador;
+
+// string
+let nombre: string = "Rafa";
+
+// arreglos
+let people: string[] = ["Isabel", "Nicole", "Dana"];
+let peopleAndNumbers: Array<string | number> = ["Dana", 3000];
+
+// enum
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
+}
+
+let colorFavorito: Color = Color.Azul;
+
+// any
+let comodin: any = "Joker";
+comodin = { type: "wildcard" };
+
+// object
+let objeto: object = { objeto: "hola" };
